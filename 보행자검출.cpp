342faa30b3321920bd4711c48c3152dc75ed34ac
SuJung Yu <87973617+sujungYu@users.noreply.github.com>
2023-03-02 15:09:42 +0900

    feat : 보행자 검출 프로그래밍

---
+++ 보행자검출.cpp
@@ -0,0 +1,38 @@
+﻿#include <opencv2/opencv.hpp>
+#include <iostream>
+using namespace cv;
+using namespace std;
+
+int main()
+{
+
+	VideoCapture cap("C:/Users/82103/source/repos/201821342/201821342/car.mp4");
+
+	if (!cap.isOpened()) {
+		cerr << "Video open failed!" << endl;
+		return -1;
+	}
+	HOGDescriptor hog;
+	hog.setSVMDetector(HOGDescriptor::getDefaultPeopleDetector());
+
+	Mat frame;
+	while (true) {
+		cap >> frame;
+		if (frame.empty())
+			break;
+
+		vector<Rect> detected;
+		hog.detectMultiScale(frame, detected);
+
+		for (Rect r : detected) {
+			Scalar c = Scalar(0, 0, 255);
+			rectangle(frame, r, c, 3);
+		}
+		imshow("frame", frame);
+
+		if (waitKey(10) == 27)
+			break;
+	}
+	destroyAllWindows();
+	return 0;
+}
